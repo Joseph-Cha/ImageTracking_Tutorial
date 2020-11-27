b67f5762f08a83a85712bee94098e6d7ad52b0a6
dhcha-virnect <dhcha@virnect.com>
2020-11-27 17:15:57 +0900

    Upload

---
+++ Assets/Script/ImageRecognitionTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageRecognitionTest : MonoBehaviour
+{
+    private ARTrackedImageManager arTrackedImageManager;
+
+    private void Awake()
+    {
+        arTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    public void OnEnable()
+    {
+        arTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    public void OnDisable()
+    {
+        arTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+
+    }
+
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach(var trackedImage in args.added)
+        {
+            Debug.Log(trackedImage.name);
+        }
+    }
+}
